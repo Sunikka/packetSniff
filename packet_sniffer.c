4cafa5ad6ec8df6c0ceaf592810174783f5eacdc
Sunikka <devsun45@gmail.com>
2025-03-20 15:47:25 +0200

    More basic functionality

--- packet_sniffer.c
+++ packet_sniffer.c
@@ -22,6 +22,21 @@ void formatIPv4(const uint32_t addr, char* buffer) {
 	  );	
 }
 
+void formatIPv6(const uint8_t addr[16], char* buffer) {
+	snprintf(buffer, 40, "%x:%x:%x:%x:%x:%x:%x:%x",
+	  (addr[0] << 8 | addr[1]),
+	  (addr[2] << 8 | addr[3]),
+	  (addr[4] << 8 | addr[5]),
+	  (addr[6] << 8 | addr[7]),
+	  (addr[8] << 8 | addr[9]),
+	  (addr[10] << 8 | addr[11]),
+	  (addr[12] << 8 | addr[13]),
+	  (addr[14] << 8 | addr[15])
+	  );
+}; 
+
+
+
 uint16_t parse_ethernet_header(const u_char *packet) {
 	struct ethernet_header *eth = (struct ethernet_header *)packet;
 	
@@ -48,18 +63,25 @@ void parse_ipv_header(const u_char *packet, uint16_t ethertype) {
 
 	} else if (ntohs(ethertype) == 0x86DD) { // if ethertype is IPv6
 		struct ipv6_header *iph = (struct ipv6_header *)packet;	
-		printf("IPv6 Support not yet added \n");
+		char destIP[40];	
+		char srcIP[40];	
+		formatIPv6(iph->dest_address, destIP);
+		formatIPv6(iph->src_address, srcIP);
+		printf("[IPv6]: %s --> %s ", srcIP, destIP);
 	}else {
 		printf("Unidentified IP version: %04x\n", ntohs(ethertype));
 
 	}
-} 
-
+}
 void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
 	printf("packet captured, length: %d \n", header->len);
 
 	uint16_t ethertype = parse_ethernet_header(packet);
-	parse_ipv_header(packet, ethertype);
+	if (ntohs(ethertype) < 0x0600) {
+		printf("IEEE 802.3 frame detected. Length: %d bytes\n", ntohs(ethertype));
+	} else {
+		parse_ipv_header(packet, ethertype);
+	}  
 	printf("\n\n");
 }
 
@@ -101,6 +123,8 @@ int main() {
 
 
 	pcap_close(handle);
+
+
 	printf("Capture complete. \n");
 	return 0;
 }
